aaf39a7cfcb473db388f3ab6dae3160dcb2a7346
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-10-10 04:05:27 +0100

    Late night learning about JavaScript

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JVSpace
 Area for java / java script testing
 Testing the github process
+
+Learned from https://www.youtube.com/watch?v=W6NZfCO5SIk
+index.html, index.js
---
+++ index.js
@@ -0,0 +1,30 @@
+//First JavaScript code after 1000 years jkjk
+console.log('Hello World!');
+//------------------------------------------------
+
+//Put meaningful name for the variables
+//Cannot be a reserved words like let, if, else, etc
+//Cannot start with a number (1name)
+//Use cammelNotation
+let firstName = 'Syahmi'; //Dont forget the semi colon on each js code!!!
+let lastName = 'Sade';
+
+console.log(firstName, lastName); 
+//--------------------------------------------------------------------------
+
+const interestRate = 0.3;
+//Remember to not change the value of constant variables!!!
+//interestRate = 0.1 
+console.log(interestRate)
+//----------------------------------------------------------
+
+let name =  'Syahmi'; // String literal
+let age = 22; // Number literal
+let isApproved = true; //Boolean literal
+let fakeName = undefined;
+let colorFav = null;
+//----------------------------------------
+
+//typeof is a reserved key word
+/* There are actually 2 categories of types which are
+    Primitives/Value types and Reference types **/ 
